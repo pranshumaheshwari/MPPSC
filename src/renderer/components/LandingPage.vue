86a9eb6cfa2be1f2ece29a43f3782e4b03e5616e
Pranshu Maheshwari <maheshwari.pranshu511@gmail.com>
2019-05-12 18:58:50 +0530

    Initial Commit

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="wrapper">
+    <h1>MPPSC Ranklist Software</h1>
+    <div class="Login row">
+      <div class="col-sm-3"></div>
+      <div class="col-sm-6">
+        <LoginForm />
+      </div>
+      <div class="col-sm-3"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import LoginForm from './LandingPage/LoginForm'
+
+  export default {
+    name: 'landing-page',
+    components: { LoginForm }
+  }
+</script>
+
+<style lang="scss">
+  #wrapper {
+    height: 100vh;
+    padding: 60px 80px;
+    width: 100vw;
+    h1 {
+      margin-left: 14vw;
+      margin-top: 5vh;
+    }
+  }
+  
+  .Login {
+    margin-top: 15%;
+  }
+</style>
---
+++ src/renderer/components/LandingPage/LoginForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <form @submit.prevent="formSubmit" class="form-horizontal">
+      <legend>
+        Login
+      </legend>
+      <div v-if="error" class="alert alert-danger">
+        {{ error }}
+      </div>
+      <div class="form-group">
+        <input type="text" placeholder="Username" class="form-control" id="username" name="username" v-model="username">
+      </div>
+      <div class="form-group">
+        <input type="password" placeholder="Password" class="form-control" id="password" name="password" v-model="password">
+      </div>
+      <input type="submit" class="btn btn-dark"> 
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        error: '',
+        username: '',
+        password: ''
+      }
+    },
+    methods: {
+      formSubmit: function () {
+        // Check For User In DB
+        // If user user exists and password is correct
+        // this.$route.push(path to next route)
+        // else this.error = error
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
